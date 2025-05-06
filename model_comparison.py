5d2046a7fede2cb1c63eda80fc13ceb43bdf779f
Tekin Gunasar <tgunasar@ucsd.edu>
2025-05-05 20:59:30 -0700

    loaders

--- model_comparison.py
+++ model_comparison.py
@@ -3,7 +3,7 @@ from torch.utils.data import DataLoader, TensorDataset
 import torch.nn.functional as F
 from torch import nn
 
-from SVHN_Helpers import load_SVHN  # Assume this returns torch tensors
+from loaders import load_SVHN,load_CIFAR10,load_FASHION_MNIST
 
 from utils import *
 
@@ -11,26 +11,25 @@ import numpy as np
 import random
 import os
 
-def set_seed(random_seed):
-    os.environ['PYTHONHASHSEED'] = str(random_seed)
-    torch.manual_seed(random_seed)
-    torch.cuda.manual_seed(random_seed)
-    torch.cuda.manual_seed_all(random_seed)
-    np.random.seed(random_seed)
-    random.seed(random_seed)
-    torch.backends.cudnn.deterministic = True
-    torch.backends.cudnn.benchmark = False
+#def set_seed(random_seed):
+#    os.environ['PYTHONHASHSEED'] = str(random_seed)
+#    torch.manual_seed(random_seed)
+#    torch.cuda.manual_seed(random_seed)
+#    torch.cuda.manual_seed_all(random_seed)
+#    np.random.seed(random_seed)
+#    random.seed(random_seed)
+#    torch.backends.cudnn.deterministic = True
+#    torch.backends.cudnn.benchmark = False
 
 
-set_seed(42)
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-class SVHN_MLP(nn.Module):
+class MLP(nn.Module):
     def __init__(self, n_classes):
         super().__init__()
         self.flatten = nn.Flatten()
-        self.hidden = nn.Linear(32 * 32 * 3, 1024)
+        self.hidden = nn.Linear(3072, 1024)
         self.dropout = nn.Dropout(0.3)
         self.output = nn.Linear(1024, n_classes)
 
@@ -40,9 +39,8 @@ class SVHN_MLP(nn.Module):
         x = self.dropout(x)
         return self.output(x)
 
-# Load data
-X_train, y_train = load_SVHN(split='train')
-X_test, y_test = load_SVHN(split='test')
+X_train, y_train, X_test, y_test = load_SVHN()
+
 
 train_dataset = TensorDataset(X_train, y_train)
 test_dataset = TensorDataset(X_test, y_test)
@@ -57,24 +55,27 @@ n_epochs = 10
 delta = 0.2
 delta_model = train_standard_model_delta(
     n_epochs = n_epochs,
-    base_model = SVHN_MLP,
+    base_model = MLP,
     loader = train_loader,
     delta = delta
 )
 
 standard_model = train_standard_model(
     n_epochs = n_epochs,
-    base_model = SVHN_MLP,
+    base_model = MLP,
     loader = train_loader
 )
 
 
 standard_model_direct_label_smoothing = train_standard_model(
     n_epochs = n_epochs,
-    base_model = SVHN_MLP,
+    base_model = MLP,
     loader = train_loader,
     loss_fn = torch.nn.CrossEntropyLoss(label_smoothing = 1-delta)
 )
+
+#i.e placing 20% confidence in the true class when constructing soft targets
+
 
 delta_test_acc = get_test_acc(
     delta_model,test_loader
@@ -83,7 +84,6 @@ delta_test_acc = get_test_acc(
 standard_test_acc = get_test_acc(
     standard_model,test_loader
 )
-
 
 standard_test_acc_label_smoothing = get_test_acc(
     standard_model_direct_label_smoothing,test_loader
